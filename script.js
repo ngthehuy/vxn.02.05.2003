8b97bfd926b413cc8af03eac6f7133e29d8689a9
unknown <huy.d21c01a0615@gmail.com>
2023-04-18 14:36:38 +0700

    comment

---
+++ script.js
@@ -0,0 +1,18 @@
+﻿const countdown = document.querySelector('.countdown');
+
+const interval = setInterval(() => {
+const dealine = new Date(2023, 4, 2, 00, 00, 00);
+const current = new Date();
+const diff = dealine - current;
+const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+const minutes = Math.floor((diff / (1000 * 60)) % 60);
+const seconds = Math.floor((diff / 1000) % 60);
+
+    countdown.innerHTML = `
+<div data-content = "Days">${days}</div>
+<div data-content = "Hours">${hours.length === 1 ? `0${hours}` : hours}</div>
+<div data-content = "Minutes">${minutes.length === 1 ? `0${minutes}` : minutes}</div>
+<div data-content = "Seconds">${seconds.length === 1 ? `0${seconds}` : seconds}</div>
+`;
+},1000)
